a54918b53a1ef0b696ef49ddbe6ddc32ac6c184f
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2020-12-16 18:49:36 -0300

    Add function to list books

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,4 @@
+
 let myLibrary = [];
 
 function Book(title, author, pages, description,read) {
@@ -9,9 +10,29 @@ function Book(title, author, pages, description,read) {
 }
 
 function addBookToLibrary(book) {
-  myLibrary.push(book)
-  console.log(myLibrary)
+  myLibrary.push(book);
 }
+
+function listBooks() {
+  var content = document.querySelector('div');
+  alert(content);
+  myLibrary.forEach (function(item) {
+    var card = document.createElement('div');
+    card.innerText = item;
+    content.appendChild(card);
+  })
+}
+
 let book= new Book("100 rules of love", "ABC", "A great book", true)
 
 addBookToLibrary(book);
+
+let book1= new Book("200 rules of love", "DBD", "A great book", true)
+
+addBookToLibrary(book1);
+
+let book2= new Book("300 rules of love", "Umair", "A great book", true)
+
+addBookToLibrary(book2);
+
+listBooks()
