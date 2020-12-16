87cce4381e67a6931a92818be89ba22a3b497490
Umair Arshad <Umair6622@gmail.com>
2020-12-17 02:08:10 +0500

    save the book object in array

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,17 @@
-window.onload = function() {
-  alert('test')
+let myLibrary = [];
+
+function Book(title, author, pages, description,read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.description = description;
+  this.read = read;
 }
+
+function addBookToLibrary(book) {
+  myLibrary.push(book)
+  console.log(myLibrary)
+}
+let book= new Book("100 rules of love", "ABC", "A great book", true)
+
+addBookToLibrary(book);
