dc339733a6ae889797412bf159c1e7146f837067
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2020-12-17 13:01:10 -0300

    Add delete book function and clearInputs function

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,5 +1,6 @@
 let myLibrary = [];
 let count = 0;
+
 function Book(title, author, pages, description, read) {
     this.title = title;
     this.author = author;
@@ -12,7 +13,14 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
     listBooks(myLibrary, count);
     count++;
-    console.log(myLibrary);
+}
+
+function clearInputs()
+{
+    let formInputs = document.querySelectorAll('input');
+    formInputs.forEach(item => item.value = '')
+    let checkbox = document.querySelector('#read');
+    checkbox.checked = false;
 }
 
 function saveNewBook(event) {
@@ -25,22 +33,48 @@ function saveNewBook(event) {
 
     book = new Book(title, author, pages, description, read);
     addBookToLibrary(book);
+
+    let modal_backdrop = document.querySelector('.modal-backdrop.show ');
+    modal_backdrop.style.display = 'none';
+    let modal = document.querySelector('.modal');
+    modal.style.display = 'none';
+
+    clearInputs();
 }
+
+function deleteBook(event) {
+    id = event.target.getAttribute('delete-id');
+    data='div[data-id="'+id+'"]';
+    let book = document.querySelector(data);
+    book.remove();
+}
+
 function listBooks(myLibrary, count) {
     var content = document.querySelector(".content");
     var card = document.createElement("div");
     card.setAttribute("class", "card");
     var card_body = document.createElement("div");
     card_body.setAttribute("class", "card-body");
-    card_body.innerText = myLibrary[count].title;
+    var card_title = document.createElement('h5');
+    card_title.setAttribute('class', 'card-title');
+    card_title.innerText = myLibrary[count].title;
     var p = document.createElement("p");
     p.setAttribute("class", "card-text");
     p.innerText = myLibrary[count].author;
     var description = document.createElement("p");
     description.setAttribute("class", "card-text");
     description.innerText = myLibrary[count].description;
+
+    let deleteButton = document.createElement('button');
+    deleteButton.setAttribute('class', 'btn btn-danger');
+    deleteButton.setAttribute('delete-id', count);
+    deleteButton.addEventListener('click', deleteBook,false);
+    deleteButton.innerText = 'Delete';
+
+    card_body.appendChild(card_title);
     card.appendChild(card_body);
     card.appendChild(p);
     card.appendChild(description);
     content.appendChild(card);
+    card.appendChild(deleteButton);
 }
